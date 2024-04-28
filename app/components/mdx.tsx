e21daad664c37162c2b08513c2c76132f26e81dd
apple <apple@AccMacbook.local>
2024-04-28 14:25:44 +0800

    feat: init project

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -6,6 +6,23 @@ import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import React from "react";
 // import { LiveCode } from './sandpack';
 
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
 function Table({ data }) {
   let headers = data.headers.map((header, index) => (
     <th key={index}>{header}</th>
@@ -43,11 +60,20 @@ function CustomLink(props) {
     return <a {...props} />;
   }
 
-  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+  return (
+    <span className="flex flex-row">
+      <a target="_blank" rel="noopener noreferrer" className="link-a" {...props} />
+      <ArrowIcon/>
+    </span>
+  );
 }
 
 function RoundedImage(props) {
   return <Image alt={props.alt} className="rounded-lg" {...props} />;
+}
+
+function CustomLi(props) {
+  return <li className="custom-blog-li" {...props} />;
 }
 
 function Callout(props) {
@@ -131,8 +157,11 @@ function slugify(str) {
 
 function createHeading(level: number) {
   const Component =({ children }) => {
+    console.log('children', children)
     let slug = slugify(children);
-    return React.createElement(`h${level}`, { id: slug }, [
+    console.log('slug', slug)
+
+    return React.createElement(`h${level}`, { id: `${slug} ` , className: `anchorH${level}`,}, [
         React.createElement("a", {
           href: `#${slug}`,
           key: `link-${slug}`,
@@ -157,6 +186,7 @@ let components = {
   Callout,
   ProsCard,
   ConsCard,
+  li: CustomLi,
   // StaticTweet: TweetComponent,
   // code: Code,
   Table,
